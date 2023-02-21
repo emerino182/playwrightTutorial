3cbc99d4e97895995fe416c1120df0b3237a5f68
Eduardo Merino <emerino@consumeraffairs.com>
2023-02-21 16:12:33 -0600

    All of these

---
+++ test-1.py
@@ -0,0 +1,29 @@
+from playwright.sync_api import Playwright, sync_playwright, expect
+
+
+def run(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    context = browser.new_context()
+    page = context.new_page()
+    page.set_default_timeout(15000)
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    page.wait_for_load_state("networkidle")
+    page.get_by_role("button", name="Log In").click()
+    page.get_by_test_id("signUp.switchToSignUp").click()
+    page.get_by_test_id("siteMembersDialogLayout").get_by_test_id("buttonElement").click()
+    page.get_by_test_id("emailAuth").get_by_label("Email").click()
+    page.get_by_test_id("emailAuth").get_by_label("Email").fill("symon.storozhenko@gmail.com")
+    page.get_by_label("Password").click()
+    page.get_by_label("Password").fill("test123", timeout=3000)
+    page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
+    page.wait_for_load_state("networkidle")
+    expect(page.get_by_role("button", name="Log In")).to_be_hidden(timeout=7000)
+    print("Yeai")
+
+    # ---------------------
+    context.close()
+    browser.close()
+
+
+with sync_playwright() as playwright:
+    run(playwright)
