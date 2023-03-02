5fb7cd0bf258208f1f7ae616db9dfbc9a559c6d6
Eduardo Merino <emerino@consumeraffairs.com>
2023-03-02 15:58:30 -0600

    PyCharm commit

---
+++ home_page_elements.py
@@ -0,0 +1,8 @@
+
+
+class HomePage:
+
+    def __init__(self, page):
+        self.celebrate_header = page.get_by_text("Celebrating Beauty and Style")
+        self.celebrate_body = page\
+            .get_by_text("playwright-practice was founded by a group of like-minded fashion devotees, dete")
--- test-1.py
+++ test-1.py
@@ -2,22 +2,35 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 
 
 def run(playwright: Playwright) -> None:
-    browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    # browser = playwright.chromium.launch(headless=False, slow_mo=500)
+    browser = playwright.chromium.launch(headless=False)
     context = browser.new_context()
     page = context.new_page()
     page.set_default_timeout(15000)
     page.goto("https://symonstorozhenko.wixsite.com/website-1")
     page.wait_for_load_state("networkidle")
     page.get_by_role("button", name="Log In").click()
+    # page.get_by_text("Log In").click()
+    # login_issue = True
+    # while login_issue:
+    #     if not page.get_by_test_id("signUp.switchToSignUp").is_visible():
+    #         page.get_by_role("button", name="Log In").click()
+    #     else:
+    #         login_issue = False
     page.get_by_test_id("signUp.switchToSignUp").click()
     page.get_by_test_id("siteMembersDialogLayout").get_by_test_id("buttonElement").click()
-    page.get_by_test_id("emailAuth").get_by_label("Email").click()
+    # page.get_by_test_id("emailAuth").get_by_label("Email").click()
+    # page.locator("input:below(:text('Email'))").fill("symon.storozhenko@gmail.com")
     page.get_by_test_id("emailAuth").get_by_label("Email").fill("symon.storozhenko@gmail.com")
-    page.get_by_label("Password").click()
+    # page.get_by_label("Password").click()
     page.get_by_label("Password").fill("test123", timeout=3000)
     page.get_by_test_id("submit").get_by_test_id("buttonElement").click()
     page.wait_for_load_state("networkidle")
-    expect(page.get_by_role("button", name="Log In")).to_be_hidden(timeout=7000)
+    expect(page.get_by_role("button", name="Log In")).to_be_hidden()
+    page.locator("// *[ @ id = 'comp-kqx7ocfp5label']").click()
+    # page.wait_for_load_state()
+    page.wait_for_selector("#comp-kqx72xkk > h1 > span")
+    expect(page.get_by_text("Help Center")).to_be_visible()
     print("Yeai")
 
     # ---------------------
