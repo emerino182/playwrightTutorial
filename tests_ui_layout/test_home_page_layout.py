179f231b436fc6d5a2c04ec76db99dc63bc9d773
Eduardo Merino <emerino@consumeraffairs.com>
2023-03-08 10:23:08 -0600

    PyCharm commit

--- tests_ui_layout/test_home_page_layout.py
+++ tests_ui_layout/test_home_page_layout.py
@@ -1,3 +1,4 @@
+import pytest
 from playwright.sync_api import Playwright, sync_playwright, expect
 from pom.home_page_elements import HomePage
 from pom.shop_women_elements import ShopWomen
@@ -12,3 +13,24 @@ def test_about_us_section_verbiage(playwright: Playwright) -> None:
     expect(home_page.celebrate_header).to_be_visible()
     expect(shop_women.celebrate_body).to_be_visible()
 
+
+@pytest.mark.skip(reason="not ready")
+def test_about_us_section_verbiage_2(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    home_page = HomePage(page)
+    shop_women = ShopWomen(page)
+    expect(home_page.celebrate_header).to_be_visible()
+    expect(shop_women.celebrate_body).to_be_visible()
+
+
+@pytest.mark.xfail(reason="url not ready")
+def test_about_us_section_verbiage_3(playwright: Playwright) -> None:
+    browser = playwright.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://symonstorozhenko.wixsite.com/website-1")
+    home_page = HomePage(page)
+    shop_women = ShopWomen(page)
+    expect(home_page.celebrate_header).to_be_visible()
+    expect(shop_women.celebrate_body).to_be_visible()
