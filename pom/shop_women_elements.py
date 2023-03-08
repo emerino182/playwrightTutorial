942ce30ba434aa8972e9604236e31b44b6819961
Eduardo Merino <emerino@consumeraffairs.com>
2023-03-08 09:58:48 -0600

    PyCharm commit

--- pom/shop_women_elements.py
+++ pom/shop_women_elements.py
@@ -5,3 +5,10 @@ class ShopWomen:
         self.celebrate_header = page.get_by_text("Celebrating Beauty and Style")
         self.celebrate_body = page\
             .get_by_text("playwright-practice was founded by a group of like-minded fashion devotees, dete")
+
+
+# NAMING CONVENTIONS
+# packages/folders : "test*";, "tests_ui_layout"
+# python files: "tests*", "test_home_page_layout"
+# python functions/modules: "test*", "test_about_us_sections_verbiage"
+# python classes: "Test*", "TestHomePage"
