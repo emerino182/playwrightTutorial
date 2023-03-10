b46a7ade14eabfed26b2898c413b57b05dc349b4
Eduardo Merino <emerino@consumeraffairs.com>
2023-03-10 13:13:37 -0600

    PyCharm commit

--- tests_ui_layout/test_home_page_layout.py
+++ tests_ui_layout/test_home_page_layout.py
@@ -8,18 +8,15 @@ from pom.shop_women_elements import ShopWomen
 def test_about_us_section_verbiage(login_set_up):
     page = login_set_up
     home_page = HomePage(page)
-    shop_women = ShopWomen(page)
-    expect(home_page.celebrate_header).to_be_visible()
-    expect(shop_women.celebrate_body).to_be_visible()
+    page.pause()
+    expect(home_page.celebrate_header).not_to_be_visible()
 
 
 @pytest.mark.skip(reason="not ready")
 def test_about_us_section_verbiage_2(login_set_up):
     page = login_set_up
     home_page = HomePage(page)
-    shop_women = ShopWomen(page)
     expect(home_page.celebrate_header).to_be_visible()
-    expect(shop_women.celebrate_body).to_be_visible()
 
 
 @pytest.mark.regression
