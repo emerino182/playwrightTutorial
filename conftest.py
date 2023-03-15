4b100803445ade712decc8554cbd718dd1e21465
Eduardo Merino <emerino@consumeraffairs.com>
2023-03-15 15:07:53 -0600

    .env changes

--- conftest.py
+++ conftest.py
@@ -1,11 +1,13 @@
 import os
 import pytest
 
-try:
-    PASSWORD = os.environ['PASSWORD']
-except KeyError:
-    import utils.secret_config
-    PASSWORD = utils.secret_config.PASSWORD
+PASSWORD = os.environ['PASSWORD']
+
+# try:
+#     PASSWORD = os.environ['PASSWORD']
+# except KeyError:
+#     import utils.secret_config
+#     PASSWORD = utils.secret_config.PASSWORD
 
 
 @pytest.fixture(scope="session")
