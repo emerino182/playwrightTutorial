323c08cf63d853d022b566682244a92c912cc86e
Eduardo Merino <emerino@consumeraffairs.com>
2023-03-16 10:48:09 -0600

    visual added

--- pom/home_page_elements.py
+++ pom/home_page_elements.py
@@ -6,3 +6,4 @@ class HomePage:
         self.celebrate_header = page.get_by_text("Celebrating Beauty and Style")
         self.celebrate_body = page\
             .get_by_text("playwright-practice was founded by a group of like-minded fashion devotees, dete")
+        self.cart_icon = page.get_by_role("button", name="Cart with 0 items")
--- visual_tests/test_landing_page_visual.py
+++ visual_tests/test_landing_page_visual.py
@@ -1,5 +1,5 @@
 from pom.home_page_elements import HomePage
-from playwright.sync_api import Playwright, sync_playwright, expect
+from playwright.sync_api import expect
 
 
 def test_visual_landing_2_1(page, assert_snapshot):
@@ -7,7 +7,7 @@ def test_visual_landing_2_1(page, assert_snapshot):
     page.goto("https://symonstorozhenko.wixsite.com/website-1/shop")
     homepage = HomePage(page)
     expect(homepage.celebrate_header).not_to_be_visible()
-    assert_snapshot(page.screenshot())
+    assert_snapshot(page.screenshot(full_page=True, mask=[homepage.cart_icon]), threshold=1, fail_fast=True)
 
 
 def test_visual_landing_2_2(page, assert_snapshot):
